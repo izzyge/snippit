4618d3dacc320cc62f25a021eea9caedcb89220c
dianalimnos <dianalim1117@gmail.com>
2018-09-08 13:49:46 -0700

    Fixed files router again.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var foldersRouter = require('./routes/folders');
+var filesRouter = require('./routes/files');
 
 var app = express();
 const session = require('express-session');
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/folders', foldersRouter);
+app.use('/files', filesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
