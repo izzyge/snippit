b1a8a5a3266c8b0958433b2536ea696a4886cd1e
natalielim <natalielim@gmail.com>
2018-09-08 09:52:24 -0700

    Added login route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// SET layout variables
+router.use( (req, res, next) => {
+  res.locals.title = "Snippit";
+  // res.locals.currentUserId = req.session.userId;
+  // res.locals.username = req.session.username;
+  next();
+});
+
+// GET home page
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+// GET login
+router.get('/login', (req, res, next) => {
+  res.render('login');
 });
 
 module.exports = router;
