a210e03360c5c7f4341f794c8b97b945f30f2650
izzyge <geisabella@gmail.com>
2018-09-08 12:03:52 -0700

    show.hbs add

--- models/folder.js
+++ models/folder.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const ProjectSchema = new Schema({
+const FolderSchema = new Schema({
   topic: { type: String, required: true },
   users: [{ type: Schema.Types.ObjectId, ref: 'User'}],
   // files: [{type: String, required: true}]
