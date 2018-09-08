39d31d40579e25a2188ccd03e4f12f6adfc91219
izzyge <geisabella@gmail.com>
2018-09-08 11:36:33 -0700

    login?

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/user');
 
 // SET layout variables
-router.use( (req, res, next) => {
+router.use( function(req, res, next) {
   res.locals.title = "Snippit";
-  // res.locals.currentUserId = req.session.userId;
+
+  res.locals.currentUserId = req.session.userId;
   // res.locals.username = req.session.username;
   next();
 });
@@ -19,32 +22,30 @@ router.get('/login', (req, res, next) => {
   res.render('login');
 });
 
-// // POST login
-// router.post('/login', (req, res, next) => {
-//   User.authenticate(req.body.username, req.body.password, (err, user) => {
-//     if (err || !user) {
-//       const next_error = new Error("Username or password incorrect");
-//       next_error.status = 401;
-//
-//       return next(next_error);
-//     } else {
-//       req.session.userId = user._id;
-//       req.session.username = user.username;
-//
-//       return res.redirect('/') ;
-//     }
-//   });
-// });
-//
-// // POST logout
-// router.get('/logout', (req, res, next) => {
-//   if (req.session) {
-//     req.session.destroy((err) => {
-//       if (err) return next(err);
-//     });
-//   }
-//
-//   return res.redirect('/');
-// });
+router.post('/login', (req, res, next) => {
+  User.authenticate(req.body.username, req.body.password, (err, user) => {
+    if (err || !user) {
+      const next_error = new Error("Username or password incorrect");
+      next_error.status = 401;
+
+      return next(next_error);
+    } else {
+      req.session.userId = user._id;
+
+      return res.redirect('/projects') ;
+    }
+  });
+});
+
+router.get('/logout', (req, res, next) => {
+  if (req.session) {
+    req.session.destroy((err) => {
+      if (err) return next(err);
+    });
+  }
+
+  return res.redirect('/login');
+});
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const auth = require('./helpers/auth');
 
 //Users index
 router.get('/', (req, res, next) => {
